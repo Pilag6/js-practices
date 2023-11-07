7bd5735666c770a9b618d94a7f05af97c24ad636
Pilag6 <pilag6@gmail.com>
2023-11-07 09:59:25 +0100

    update small and large num in an array

--- js/findSmallLarge.js
+++ js/findSmallLarge.js
@@ -1,13 +1,88 @@
 // Find the Smallest and Largest Numbers in an Array
 
+// With sort() method
+
+// let numberArray = [40, 500, 1, 2350, 5, 25, 10];
+
+// let numsOrd = numberArray.sort((a, b) => a - b);
+
+// console.log(numsOrd);
+
+// let small = numsOrd[0];
+// let large = numsOrd[numsOrd.length - 1];
+
+// console.log(`The Smallest number in the array is = ${small}`);
+// console.log(`The Largest number in the array is = ${large}`);
+
+//----------------------------------------------------------------------
+
+// With Math.min() and Math.max() methods
+
+// let numberArray = [40, 500, 1, 2350, 5, 25, 10];
+
+// let small = Math.min(...numberArray);
+
+// let large = Math.max(...numberArray);
+
+// console.log(`The Smallest number in the array is = ${small}`);
+// console.log(`The Largest number in the array is = ${large}`);
+
+//----------------------------------------------------------------------
+
+// With for loop
+
 let numberArray = [40, 500, 1, 2350, 5, 25, 10];
 
-let numsOrd = numberArray.sort((a, b) => a - b);
+let small = numberArray[0];
+let large = numberArray[0];
 
-console.log(numsOrd);
-
-let small = numsOrd[0];
-let large = numsOrd[numsOrd.length - 1];
+for (let i = 0; i < numberArray.length; i++) {
+    if (numberArray[i] < small) {
+        small = numberArray[i];
+    } else if (numberArray[i] > large) {
+        large = numberArray[i];
+    }
+}
 
 console.log(`The Smallest number in the array is = ${small}`);
 console.log(`The Largest number in the array is = ${large}`);
+
+//----------------------------------------------------------------------
+
+// With forEach() method
+
+// let numberArray = [40, 500, 1, 2350, 5, 25, 10];
+
+// let small = numberArray[0];
+// let large = numberArray[0];
+
+// numberArray.forEach((num) => {
+//     if (num < small) {
+//         small = num;
+//     } else if (num > large) {
+//         large = num;
+//     }
+// });
+
+// console.log(`The Smallest number in the array is = ${small}`);
+// console.log(`The Largest number in the array is = ${large}`);
+
+//----------------------------------------------------------------------
+
+// With reduce() method
+
+// let numberArray = [40, 500, 1, 2350, 5, 25, 10];
+
+// let small = numberArray.reduce((a, b) => {
+//     return Math.min(a, b);
+// });
+
+// let large = numberArray.reduce((a, b) => {
+//     return Math.max(a, b);
+// });
+
+// console.log(`The Smallest number in the array is = ${small}`);
+// console.log(`The Largest number in the array is = ${large}`);
+
+//----------------------------------------------------------------------
+
